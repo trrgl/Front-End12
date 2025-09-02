ab5461a8a9dbc297e7020c1d554f101db707875b
Tergel Ganbold <trrgl@Tergels-MacBook-Air.local>
2025-09-02 13:21:37 +0800

    desc

--- src/pages/7secondstoglory.js
+++ src/pages/7secondstoglory.js
@@ -9,7 +9,7 @@ export default function Chall1() {
                 <h1 className="text-xl"><span className="text-4xl font-bold">7 Seconds to glory </span> by: fg0x0</h1>
                 <h1>1. Өгөгдсөн `bomb` ELF file-ийг compiler-аар задална.</h1>
                 <img src="/7secondstoglory/img1.png"/>
-                <h1>2. Олсон function нь byte_4A1100 дээр байгаа дата-г 0x25-аар XOR-дож байгаа буцаан XOR-доод гарсан утгын програмлуугаа оруулна.</h1>
+                <h1>2. Олсон sub_401909 дээр байгаа function нь byte_4A1100 дээр байгаа дата-г 0x25-аар XOR-дож байгаа буцаан XOR-доод гарсан утгын програмлуугаа оруулна.</h1>
                 <img src="/7secondstoglory/img2.png"/>
                 <img src="/7secondstoglory/img3.png"/>
                 <img src="/7secondstoglory/img4.png"/>
